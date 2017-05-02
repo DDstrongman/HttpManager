d57cb67820f8a9f9e9826602fd7b5b9bcced0df7
sanli <lishengshu@xinxindai.com>
2017-05-02 16:04:32 +0800

    1.1.1

--- HttpSupportSpec/Classes/DDHttpManager.h
+++ HttpSupportSpec/Classes/DDHttpManager.h
@@ -20,11 +20,71 @@ typedef void (^FailedBlock)(id obj);
 
 @interface DDHttpManager : NSObject
 
+/**
+ 所有http请求通用的header设置字典，比如：@"Content-Type":@"application/json; charset=utf-8"，默认不传;
+ 在发出http请求前，如果要设置通用头属性，必须先设置此处属性，否则无法成功设置通用头属性
+ */
+@property (nonatomic, strong) NSDictionary *commonHeader;
+
 + (DDHttpManager *)ShareInstance;
 #pragma afnetwork Get
-- (void)AFNetGETSupport:(NSString *)url Parameters:(NSDictionary *)dic SucessBlock:(void (^)(id))success FailedBlock:(void (^)(NSError *))failure;
+/**
+ 发起get的http访问，异步
+
+ @param url 访问的url
+ @param dic 访问的参数字典
+ @param success 成功后的处理block
+ @param failure 失败后的处理block
+ */
+- (void)AFNetGETSupport:(NSString *)url
+             Parameters:(NSDictionary *)dic
+            SucessBlock:(void (^)(id))success
+            FailedBlock:(void (^)(NSError *))failure;
+/**
+ 发起get的http访问，异步
+ 
+ @param url 访问的url
+ @param dic 访问的参数字典
+ @param header 新增http头的参数字典
+ @param success 成功后的处理block
+ @param failure 失败后的处理block
+ */
+- (void)AFNetGETSupport:(NSString *)url
+             Parameters:(NSDictionary *)dic
+              HeaderDic:(NSDictionary *)header
+            SucessBlock:(void (^)(id))success
+            FailedBlock:(void (^)(NSError *))failure;
 #pragma afnetwork Post
-- (void)AFNetPOSTSupport:(NSString *)url Parameters:(NSDictionary *)dic ConstructingBodyWithBlock:(void(^)(id<AFMultipartFormData> formData))bodyblock SucessBlock:(void (^)(id))success FailedBlock:(void (^)(NSError *))failure;
+/**
+ 发起post的http访问，muldata传输参数，即表单提交的方式
+
+ @param url http路径，string格式
+ @param dic 参数字典
+ @param bodyblock 过程中的处理block
+ @param success 成功后回调block
+ @param failure 失败后的回调block
+ */
+- (void)AFNetPOSTSupport:(NSString *)url
+              Parameters:(NSDictionary *)dic
+ConstructingBodyWithBlock:(void(^)(id<AFMultipartFormData> formData))bodyblock
+             SucessBlock:(void (^)(id))success
+             FailedBlock:(void (^)(NSError *))failure;
+/**
+ 发起post的http访问，并添加新的http头参数，muldata传输参数，即表单提交的方式
+ 
+ @param url http路径，string格式
+ @param dic 参数字典
+ @param header 新增http头的参数字典
+ @param bodyblock 过程中的处理block
+ @param success 成功后回调block
+ @param failure 失败后的回调block
+ */
+- (void)AFNetPOSTSupport:(NSString *)url
+              Parameters:(NSDictionary *)dic
+                  Header:(NSDictionary *)header
+ConstructingBodyWithBlock:(void(^)(id<AFMultipartFormData> formData))bodyblock
+             SucessBlock:(void (^)(id))success
+             FailedBlock:(void (^)(NSError *))failure;
 
 /**
  因为afnetwork默认用muldata的格式传输，所以改传输方式
@@ -34,9 +94,9 @@ typedef void (^FailedBlock)(id obj);
  @param success 成功后回调block
  @param failure 失败后的回调block
  */
-- (void)AFNetUrlPOSTSupport:(NSString *)url Parameters:(NSDictionary *)dic SucessBlock:(void (^)(id))success FailedBlock:(void (^)(NSError *))failure;
-#pragma 同步缓存get请求
-- (NSData *)httpGetSupport:(NSString *)url;
-- (NSData *)httpPostSupport:(NSString *)urlString PostName:(NSData *)fileData FileType:(NSString *)fileType FileTrail:(NSString *)fileTrail;
+- (void)AFNetUrlPOSTSupport:(NSString *)url
+                 Parameters:(NSDictionary *)dic
+                SucessBlock:(void (^)(id))success
+                FailedBlock:(void (^)(NSError *))failure;
 
 @end
--- HttpSupportSpec/Classes/DDHttpManager.m
+++ HttpSupportSpec/Classes/DDHttpManager.m
@@ -18,9 +18,37 @@
     });
     return sharedHttpManagerInstance;
 }
+#pragma mark - methods
+- (void)commonHeadersForHttp:(AFHTTPSessionManager *)manager {
+    if (self.commonHeader) {
+        manager.requestSerializer = [AFHTTPRequestSerializer new];
+        [[self.commonHeader allKeys] enumerateObjectsUsingBlock:^(NSString *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            [manager.requestSerializer setValue:self.commonHeader[obj] forHTTPHeaderField:obj];
+        }];
+    }
+}
 
-- (void)AFNetGETSupport:(NSString *)url Parameters:(NSDictionary *)dic SucessBlock:(void (^)(id))success FailedBlock:(void (^)(NSError *))failure {
+- (void)AFNetGETSupport:(NSString *)url
+             Parameters:(NSDictionary *)dic
+            SucessBlock:(void (^)(id))success
+            FailedBlock:(void (^)(NSError *))failure {
+    [self AFNetGETSupport:url
+               Parameters:dic
+                HeaderDic:nil
+              SucessBlock:success
+              FailedBlock:failure];
+}
+
+- (void)AFNetGETSupport:(NSString *)url
+             Parameters:(NSDictionary *)dic
+              HeaderDic:(NSDictionary *)header
+            SucessBlock:(void (^)(id))success
+            FailedBlock:(void (^)(NSError *))failure {
     AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
+    [self commonHeadersForHttp:session];
+    [self addHttpHeader:header
+              AFManager:session];
+    
     session.responseSerializer = [AFHTTPResponseSerializer serializer];
     
     [session.responseSerializer.acceptableContentTypes setByAddingObject:@"text/html"];
@@ -29,19 +57,44 @@
     session.requestSerializer.timeoutInterval = 60;
     url = [url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     
-    [session GET:url parameters:dic progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        if (success) {
-            success(responseObject);
-        }
-    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        if (failure) {
-            failure(error);
-        }
-    }];
+    [session GET:url
+      parameters:dic
+        progress:nil
+         success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+             if (success) {
+                 success(responseObject);
+             }
+         }
+         failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+             if (failure) {
+                 failure(error);
+             }
+         }];
 }
 #pragma 没有加入progressBlock,需要过程中处理的话加入progressBlock
-- (void)AFNetPOSTSupport:(NSString *)url Parameters:(NSDictionary *)dic ConstructingBodyWithBlock:(void(^)(id<AFMultipartFormData> formData))bodyblock SucessBlock:(void (^)(id))success FailedBlock:(void (^)(NSError *))failure {
+- (void)AFNetPOSTSupport:(NSString *)url
+              Parameters:(NSDictionary *)dic
+ConstructingBodyWithBlock:(void(^)(id<AFMultipartFormData> formData))bodyblock
+             SucessBlock:(void (^)(id))success
+             FailedBlock:(void (^)(NSError *))failure {
+    [self AFNetPOSTSupport:url
+                Parameters:dic
+                    Header:nil
+ ConstructingBodyWithBlock:bodyblock
+               SucessBlock:success
+               FailedBlock:failure];
+}
+
+- (void)AFNetPOSTSupport:(NSString *)url
+              Parameters:(NSDictionary *)dic
+                  Header:(NSDictionary *)header
+ConstructingBodyWithBlock:(void(^)(id<AFMultipartFormData> formData))bodyblock
+             SucessBlock:(void (^)(id))success
+             FailedBlock:(void (^)(NSError *))failure {
     AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
+    [self commonHeadersForHttp:session];
+    [self addHttpHeader:header
+              AFManager:session];
     
     session.responseSerializer = [AFHTTPResponseSerializer serializer];
     
@@ -67,15 +120,22 @@
     }];
 }
 #pragma mark - 因为afnetwork默认用muldata的格式传输，所以改传输方式
-- (void)AFNetUrlPOSTSupport:(NSString *)url Parameters:(NSDictionary *)dic SucessBlock:(void (^)(id))success FailedBlock:(void (^)(NSError *))failure {
+- (void)AFNetUrlPOSTSupport:(NSString *)url
+                 Parameters:(NSDictionary *)dic
+                SucessBlock:(void (^)(id))success
+                FailedBlock:(void (^)(NSError *))failure {
     if ([[UIDevice currentDevice].systemVersion floatValue] >= 9.0) {
         url = [url stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
     }else {
         url = [url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     }
-    NSMutableURLRequest* formRequest = [[AFHTTPRequestSerializer serializer] requestWithMethod:@"POST" URLString:url parameters:dic error:nil];
+    NSMutableURLRequest *formRequest = [[AFHTTPRequestSerializer serializer] requestWithMethod:@"POST"
+                                                                                     URLString:url
+                                                                                    parameters:dic
+                                                                                         error:nil];
     [formRequest setValue:@"application/x-www-form-urlencoded; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    [self commonHeadersForHttp:manager];
     
     AFJSONResponseSerializer *responseSerializer = [AFJSONResponseSerializer serializer];
     
@@ -95,88 +155,7 @@
     
     [dataTask resume];
 }
-
-- (NSData *)httpGetSupport:(NSString *)urlString{
-    //第一步，创建URL
-    if ([[UIDevice currentDevice].systemVersion floatValue] >= 9.0) {
-        urlString = [urlString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
-    }else {
-        urlString = [urlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    }
-    NSURL *url = [NSURL URLWithString:urlString];
-    //第二步，通过URL创建网络请求
-    NSURLRequest *request = [[NSURLRequest alloc]initWithURL:url cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:3];
-    //NSURLRequest初始化方法第一个参数：请求访问路径，第二个参数：缓存协议，第三个参数：网络请求超时时间（秒）
-    
-    //            其中缓存协议是个枚举类型包含：
-    //
-    //            NSURLRequestUseProtocolCachePolicy（基础策略）
-    //
-    //            NSURLRequestReloadIgnoringLocalCacheData（忽略本地缓存）
-    //
-    //            NSURLRequestReturnCacheDataElseLoad（首先使用缓存，如果没有本地缓存，才从原地址下载）
-    //
-    //            NSURLRequestReturnCacheDataDontLoad（使用本地缓存，从不下载，如果本地没有缓存，则请求失败，此策略多用于离线操作）
-    //
-    //            NSURLRequestReloadIgnoringLocalAndRemoteCacheData（无视任何缓存策略，无论是本地的还是远程的，总是从原地址重新下载）
-    //
-    //            NSURLRequestReloadRevalidatingCacheData（如果本地缓存是有效的则不下载，其他任何情况都从原地址重新下载）
-    //第三步，连接服务器
-    NSData *received = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
-    return received;
-}
-
-- (NSData *)httpPostSupport:(NSString *)urlString PostName:(NSData *)fileData FileType:(NSString *)fileType FileTrail:(NSString *)fileTrail{
-    NSString *TWITTERFON_FORM_BOUNDARY = @"AaB03x";
-    if ([[UIDevice currentDevice].systemVersion floatValue] >= 9.0) {
-        urlString = [urlString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
-    }else {
-        urlString = [urlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    }
-    //根据url初始化request
-    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlString]
-                                                           cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
-                                                       timeoutInterval:3];
-    //分界线 --AaB03x
-    NSString *MPboundary=[[NSString alloc]initWithFormat:@"--%@",TWITTERFON_FORM_BOUNDARY];
-    //结束符 AaB03x--
-    NSString *endMPboundary=[[NSString alloc]initWithFormat:@"%@--",MPboundary];
-    //http body的字符串
-    NSMutableString *body=[[NSMutableString alloc]init];
-    
-    ////添加分界线，换行
-    [body appendFormat:@"%@\r\n",MPboundary];
-    //声明pic字段，文件名为boris.png
-    [body appendFormat:[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"%@%@\"\r\n",fileTrail,[self gettime],@""]];
-    //声明上传文件的格式
-    [body appendFormat:[NSString stringWithFormat:@"Content-Type: %@\r\n\r\n",fileType]];
-    
-    //声明结束符：--AaB03x--
-    NSString *end=[[NSString alloc]initWithFormat:@"\r\n%@",endMPboundary];
-    //声明myRequestData，用来放入http body
-    NSMutableData *myRequestData=[NSMutableData data];
-    //将body字符串转化为UTF8格式的二进制
-    [myRequestData appendData:[body dataUsingEncoding:NSUTF8StringEncoding]];
-    //将image的data加入
-    [myRequestData appendData:fileData];
-    //加入结束符--AaB03x--
-    [myRequestData appendData:[end dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    //设置HTTPHeader中Content-Type的值
-    NSString *content=[[NSString alloc]initWithFormat:@"multipart/form-data; boundary=%@",TWITTERFON_FORM_BOUNDARY];
-    //设置HTTPHeader
-    [request setValue:content forHTTPHeaderField:@"Content-Type"];
-    //设置Content-Length
-    [request setValue:[NSString stringWithFormat:@"%d", [myRequestData length]] forHTTPHeaderField:@"Content-Length"];
-    //设置http body
-    [request setHTTPBody:myRequestData];
-    //http method
-    [request setHTTPMethod:@"POST"];
-    //第三步，连接服务器
-    NSData *received = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
-    return received;
-}
-
+#pragma mark - support methods
 - (NSString *)gettime {
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     //设定时间格式,这里可以设置成自己需要的格式
@@ -189,16 +168,19 @@
     return newdate;
 }
 
-
-
 /**
- AFNetwork设置http的头，当是get方法时，参数代入header,当是post方法时，只用设置基本用户信息
+ 新增AFNetwork设置http的头
  
- @param headerDic get方式时，传给服务器的参数字典
+ @param header 新增的http头的参数字典
  @param manager 设置头函数时候的对象
  */
-- (void)initHttpHeader:(NSDictionary *)headerDic AFManager:(AFHTTPSessionManager *)manager {
-    
+- (void)addHttpHeader:(NSDictionary *)header AFManager:(AFHTTPSessionManager *)manager {
+    if (header) {
+        [[header allKeys] enumerateObjectsUsingBlock:^(NSString *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            [manager.requestSerializer setValue:header[obj]
+                             forHTTPHeaderField:obj];
+        }];
+    }
 }
 
 @end

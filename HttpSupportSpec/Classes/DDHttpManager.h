419e235ad7b76b720e5100b812444e7e5c81a1ca
DiDi <lishengshu248@gmail.com>
2018-08-24 10:27:36 +0800

    modify response

--- HttpSupportSpec/Classes/DDHttpManager.h
+++ HttpSupportSpec/Classes/DDHttpManager.h
@@ -5,7 +5,6 @@
 //  Created by 李胜书 on 15/8/21.
 //  Copyright (c) 2015年 李胜书. All rights reserved.
 //
-
 typedef enum {
     DDHttpGet = 0,
     DDHttpPost,
@@ -20,17 +19,6 @@ typedef enum {
     DDRequestPlist
 }DDRequestType;
 
-typedef enum {
-    ///有缓存只用缓存数据，没有则去http请求并在之后更新数据和本地缓存，默认采用此策略
-    DDCacheElseHttp = 0,
-    ///有缓存优先用缓存数据，同时去http请求并在之后更新数据和本地缓存
-    DDCacheThenHttp,
-    ///只在缓存里找，没有缓存则返回空，多用于无网环境或省流量
-    DDCacheMemoryOnly,
-    ///只在使用http访问数据，并会更新或新建本地缓存，多用于实时性要求较多的环境
-    DDHttpOnly
-}DDHttpCacheMethod;
-
 #import <Foundation/Foundation.h>
 #import <AFNetworking/AFNetworking.h>
 
@@ -41,18 +29,9 @@ typedef enum {
  在发出http请求前，如果要设置通用头属性，必须先设置此处属性，否则无法成功设置通用头属性
  */
 @property (nonatomic, strong) NSDictionary *commonHeader;
-/**
- 默认采用的缓存模式，不设置则默认DDCacheElseHttp
- */
-@property (nonatomic, assign) DDHttpCacheMethod cacheMethod;
 
-/**
- 单实例化
-
- @return 返回单实例
- */
 + (DDHttpManager *)ShareInstance;
-#pragma mark - afnetwork Get,Post(仅限不包含bodyblock的情况下，一般情况下为上传文件的时候需要bodyblock),Put,Patch,Delete,此块http有缓存
+#pragma afnetwork Get,Post(仅限不包含bodyblock的情况下，一般情况下为上传文件的时候需要bodyblock),Put,Patch,Delete
 /**
  发起所有方式的http访问，不新增头参数，请注意，POST方法仅限不需要bodyblock的情况下使用
  
@@ -67,24 +46,6 @@ typedef enum {
                  Parameters:(NSDictionary *)dic
                      Method:(DDHttpMethodType)method
               RequestMethod:(DDRequestType)request
-                SucessBlock:(void (^)(id))success
-                FailedBlock:(void (^)(NSError *))failure;
-/**
- 发起所有方式的http访问，不新增头参数，请注意，POST方法仅限不需要bodyblock的情况下使用
- 
- @param url 访问的url
- @param dic 访问的参数字典
- @param method 使用方法的type
- @param request http请求方法的type
- @param cache http缓存策略
- @param success 成功后的处理block
- @param failure 失败后的处理block
- */
-- (void)AFNetMethodsSupport:(NSString *)url
-                 Parameters:(NSDictionary *)dic
-                     Method:(DDHttpMethodType)method
-              RequestMethod:(DDRequestType)request
-                CacheMethod:(DDHttpCacheMethod)cache
                 SucessBlock:(void (^)(id))success
                 FailedBlock:(void (^)(NSError *))failure;
 /**
@@ -105,27 +66,7 @@ typedef enum {
                   HeaderDic:(NSDictionary *)header
                 SucessBlock:(void (^)(id))success
                 FailedBlock:(void (^)(NSError *))failure;
-/**
- 发起所有方式的http访问，新增头参数，请注意，POST方法仅限不需要bodyblock的情况下使用
- 
- @param url 访问的url
- @param dic 访问的参数字典
- @param method 使用方法的type
- @param request http请求方法的type
- @param cache http缓存策略
- @param header 新增http头的参数字典
- @param success 成功后的处理block
- @param failure 失败后的处理block
- */
-- (void)AFNetMethodsSupport:(NSString *)url
-                 Parameters:(NSDictionary *)dic
-                     Method:(DDHttpMethodType)method
-              RequestMethod:(DDRequestType)request
-                CacheMethod:(DDHttpCacheMethod)cache
-                  HeaderDic:(NSDictionary *)header
-                SucessBlock:(void (^)(id))success
-                FailedBlock:(void (^)(NSError *))failure;
-#pragma mark - afnetwork Post(包含bodyblock的情况下，一般情况下为上传文件的时候需要bodyblock，此块http没有缓存)
+#pragma afnetwork Post(包含bodyblock的情况下，一般情况下为上传文件的时候需要bodyblock)
 /**
  发起post的http访问，muldata传输参数，即表单提交的方式
  
--- HttpSupportSpec/Classes/DDHttpManager.m
+++ HttpSupportSpec/Classes/DDHttpManager.m
@@ -7,22 +7,10 @@
 //
 
 #import "DDHttpManager.h"
-#import "YYCache.h"
-
-#define DDWS(weakSelf)  __weak __typeof(&*self)weakSelf = self;
-#define DDSS(strongSelf)  __strong __typeof(weakSelf)strongSelf = weakSelf;
-#define DDHttpCacheName @"DDHttpYYCache"
-@interface DDHttpManager ()
-
-{
-    YYCache *httpCache;
-}
-
-@end;
 
 @implementation DDHttpManager
 
-+ (DDHttpManager *)ShareInstance {
++ (DDHttpManager *) ShareInstance {
     static DDHttpManager *sharedHttpManagerInstance = nil;
     static dispatch_once_t predicate;
     dispatch_once(&predicate, ^{
@@ -33,8 +21,8 @@
 #pragma mark - methods
 - (void)commonHeadersForHttp:(AFHTTPSessionManager *)manager {
     if (_commonHeader) {
-        [_commonHeader enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *obj, BOOL * _Nonnull stop) {
-            [manager.requestSerializer setValue:obj forHTTPHeaderField:key];
+        [[_commonHeader allKeys] enumerateObjectsUsingBlock:^(NSString *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            [manager.requestSerializer setValue:_commonHeader[obj] forHTTPHeaderField:obj];
         }];
     }
 }
@@ -49,24 +37,6 @@
                    Parameters:dic
                        Method:method
                 RequestMethod:request
-                  CacheMethod:_cacheMethod
-                    HeaderDic:nil
-                  SucessBlock:success
-                  FailedBlock:failure];
-}
-
-- (void)AFNetMethodsSupport:(NSString *)url
-                 Parameters:(NSDictionary *)dic
-                     Method:(DDHttpMethodType)method
-              RequestMethod:(DDRequestType)request
-                CacheMethod:(DDHttpCacheMethod)cache
-                SucessBlock:(void (^)(id))success
-                FailedBlock:(void (^)(NSError *))failure {
-    [self AFNetMethodsSupport:url
-                   Parameters:dic
-                       Method:method
-                RequestMethod:request
-                  CacheMethod:cache
                     HeaderDic:nil
                   SucessBlock:success
                   FailedBlock:failure];
@@ -79,190 +49,118 @@
                   HeaderDic:(NSDictionary *)header
                 SucessBlock:(void (^)(id))success
                 FailedBlock:(void (^)(NSError *))failure {
-    [self AFNetMethodsSupport:url
-                   Parameters:dic
-                       Method:method
-                RequestMethod:request
-                  CacheMethod:_cacheMethod
-                    HeaderDic:header
-                  SucessBlock:success
-                  FailedBlock:failure];
-}
-
-- (void)AFNetMethodsSupport:(NSString *)url
-                 Parameters:(NSDictionary *)dic
-                     Method:(DDHttpMethodType)method
-              RequestMethod:(DDRequestType)request
-                CacheMethod:(DDHttpCacheMethod)cache
-                  HeaderDic:(NSDictionary *)header
-                SucessBlock:(void (^)(id))success
-                FailedBlock:(void (^)(NSError *))failure {
-    BOOL needHttp = NO;
-    BOOL needCache = NO;
-    switch (cache) {
-        case DDCacheElseHttp: {
-            if (!httpCache) {
-                httpCache = [[YYCache alloc]initWithName:DDHttpCacheName];
-            }
-            if ([httpCache containsObjectForKey:url]) {
-                needCache = YES;
-            }else {
-                needHttp = YES;
-            }
-        }
-            break;
-        case DDCacheThenHttp: {
-            needCache = YES;
-            needHttp = YES;
-        }
-            break;
-        case DDCacheMemoryOnly: {
-            needCache = YES;
-        }
-            break;
-        case DDHttpOnly: {
-            needHttp = YES;
-        }
-            break;
-        default: {
-            if (!httpCache) {
-                httpCache = [[YYCache alloc]initWithName:DDHttpCacheName];
-            }
-            if ([httpCache containsObjectForKey:url]) {
-                needCache = YES;
-            }else {
-                needHttp = YES;
-            }
-        }
-            break;
-    }
-    if (needCache) {
-        if (!httpCache) {
-            httpCache = [[YYCache alloc]initWithName:DDHttpCacheName];
-        }
-        if ([httpCache containsObjectForKey:url]) {
-            [self handleSucessBlock:success
-                           BlockObj:[httpCache objectForKey:url]
-                                Url:url];
-        }
-    }
-    if (needHttp) {
-        AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
-        
-        [self initRequestType:session
-                  RequestType:request];
-        [self commonHeadersForHttp:session];
-        [self addHttpHeader:header
-                  AFManager:session];
-        
-        [session.responseSerializer.acceptableContentTypes setByAddingObject:@"text/html"];
-        url = [url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-        DDWS(weakSelf)
-        switch (method) {
-            case DDHttpGet: {
-                [session GET:url
-                  parameters:dic
-                    progress:nil
-                     success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-                         DDSS(strongSelf)
-                         [strongSelf handleSucessBlock:success
-                                              BlockObj:responseObject
-                                                   Url:url];
+    AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
+    
+    [self initRequestType:session RequestType:request];
+    
+    [self commonHeadersForHttp:session];
+    [self addHttpHeader:header
+              AFManager:session];
+    
+    session.responseSerializer = [AFHTTPResponseSerializer serializer];
+    [session.responseSerializer.acceptableContentTypes setByAddingObject:@"text/html"];
+    
+    url = [url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    
+    switch (method) {
+        case DDHttpGet: {
+            [session GET:url
+              parameters:dic
+                progress:nil
+                 success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                     if (success) {
+                         success(responseObject);
                      }
-                     failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                         if (failure) {
-                             failure(error);
-                         }
-                     }];
-            }
-                break;
-            case DDHttpPost: {
-                [session POST:url
-                   parameters:dic
-                     progress:nil
-                      success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-                          DDSS(strongSelf)
-                          [strongSelf handleSucessBlock:success
-                                               BlockObj:responseObject
-                                                    Url:url];
+                 }
+                 failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                     if (failure) {
+                         failure(error);
+                     }
+                 }];
+        }
+            break;
+        case DDHttpPost: {
+            [session POST:url
+               parameters:dic
+                 progress:nil
+                  success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                      if (success) {
+                          success(responseObject);
                       }
-                      failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                          if (failure) {
-                              failure(error);
-                          }
-                      }];
-            }
-                break;
-            case DDHttpPut: {
-                [session PUT:url
-                  parameters:dic
-                     success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-                         DDSS(strongSelf)
-                         [strongSelf handleSucessBlock:success
-                                              BlockObj:responseObject
-                                                   Url:url];
-                     }
-                     failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                         if (failure) {
-                             failure(error);
-                         }
-                     }];
-            }
-                break;
-            case DDHttpPatch: {
-                [session PATCH:url
-                    parameters:dic
-                       success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-                           DDSS(strongSelf)
-                           [strongSelf handleSucessBlock:success
-                                                BlockObj:responseObject
-                                                     Url:url];
-                       }
-                       failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                           if (failure) {
-                               failure(error);
-                           }
-                       }];
-            }
-                break;
-            case DDHttpDelete: {
-                session.requestSerializer.HTTPMethodsEncodingParametersInURI = [NSSet setWithObjects:@"GET", @"HEAD", nil];
-                [session DELETE:url
-                     parameters:dic
-                        success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-                            DDSS(strongSelf)
-                            [strongSelf handleSucessBlock:success
-                                                 BlockObj:responseObject
-                                                      Url:url];
-                        }
-                        failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                            if (failure) {
-                                failure(error);
-                            }
-                        }];
-            }
-                break;
-            default: {
-                [session GET:url
-                  parameters:dic
-                    progress:nil
-                     success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-                         DDSS(strongSelf)
-                         [strongSelf handleSucessBlock:success
-                                              BlockObj:responseObject
-                                                   Url:url];
-                     }
-                     failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                         if (failure) {
-                             failure(error);
-                         }
-                     }];
-            }
-                break;
+                  }
+                  failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                      if (failure) {
+                          failure(error);
+                      }
+                  }];
         }
+            break;
+        case DDHttpPut: {
+            [session PUT:url
+              parameters:dic
+                 success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                     if (success) {
+                         success(responseObject);
+                     }
+                 }
+                 failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                     if (failure) {
+                         failure(error);
+                     }
+                 }];
+        }
+            break;
+        case DDHttpPatch: {
+            [session PATCH:url
+                parameters:dic
+                   success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                       if (success) {
+                           success(responseObject);
+                       }
+                   }
+                   failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                       if (failure) {
+                           failure(error);
+                       }
+                   }];
+        }
+            break;
+        case DDHttpDelete: {
+            session.requestSerializer.HTTPMethodsEncodingParametersInURI = [NSSet setWithObjects:@"GET", @"HEAD", nil];
+            [session DELETE:url
+                 parameters:dic
+                    success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                        if (success) {
+                            success(responseObject);
+                        }
+                    }
+                    failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                        if (failure) {
+                            failure(error);
+                        }
+                    }];
+        }
+            break;
+            
+        default: {
+            [session GET:url
+              parameters:dic
+                progress:nil
+                 success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                     if (success) {
+                         success(responseObject);
+                     }
+                 }
+                 failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                     if (failure) {
+                         failure(error);
+                     }
+                 }];
+        }
+            break;
     }
 }
-#pragma mark - 没有加入progressBlock,需要过程中处理的话加入progressBlock
+#pragma 没有加入progressBlock,需要过程中处理的话加入progressBlock
 - (void)AFNetPOSTSupport:(NSString *)url
               Parameters:(NSDictionary *)dic
            RequestMethod:(DDRequestType)request
@@ -302,6 +200,7 @@ ConstructingBodyWithBlock:(void(^)(id<AFMultipartFormData> formData))bodyblock
     }else {
         url = [url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     }
+    
     [session POST:url
        parameters:dic
 constructingBodyWithBlock:bodyblock
@@ -309,7 +208,7 @@ constructingBodyWithBlock:bodyblock
           success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
               if (success) {
                   success(responseObject);
-              };
+              }
           }
           failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
               if (failure) {
@@ -317,7 +216,7 @@ constructingBodyWithBlock:bodyblock
               }
           }];
 }
-#pragma mark - 因为afnetwork默认用muldata的格式传输，所以改传输方式为URL
+#pragma mark - 因为afnetwork默认用muldata的格式传输，所以改传输方式
 - (void)AFNetUrlPOSTSupport:(NSString *)url
                  Parameters:(NSDictionary *)dic
                 SucessBlock:(void (^)(id))success
@@ -358,13 +257,15 @@ constructingBodyWithBlock:bodyblock
 }
 #pragma mark - support methods
 - (NSString *)gettime {
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     //设定时间格式,这里可以设置成自己需要的格式
     [dateFormatter setDateFormat:@"yyyy-MM-dd HH-mm-ss"];
     NSString *currentDateStr = [dateFormatter stringFromDate:[NSDate date]];
+    
     NSString *strUrl = [currentDateStr stringByReplacingOccurrencesOfString:@"-" withString:@""];
-    NSString *newDate = [strUrl substringToIndex:8];
-    return newDate;
+    
+    NSString *newdate=[strUrl substringToIndex:8];
+    return newdate;
 }
 
 /**
@@ -373,8 +274,7 @@ constructingBodyWithBlock:bodyblock
  @param header 新增的http头的参数字典
  @param manager 设置头函数时候的对象
  */
-- (void)addHttpHeader:(NSDictionary *)header
-            AFManager:(AFHTTPSessionManager *)manager {
+- (void)addHttpHeader:(NSDictionary *)header AFManager:(AFHTTPSessionManager *)manager {
     if (header) {
         [[header allKeys] enumerateObjectsUsingBlock:^(NSString *obj, NSUInteger idx, BOOL * _Nonnull stop) {
             [manager.requestSerializer setValue:header[obj]
@@ -389,8 +289,7 @@ constructingBodyWithBlock:bodyblock
  @param session af的主体
  @param request request的种类
  */
-- (void)initRequestType:(AFHTTPSessionManager *)session
-            RequestType:(DDRequestType)request {
+- (void)initRequestType:(AFHTTPSessionManager *)session RequestType:(DDRequestType)request {
     switch (request) {
         case DDRequestHttp:
             session.requestSerializer = [AFHTTPRequestSerializer serializer];
@@ -407,27 +306,6 @@ constructingBodyWithBlock:bodyblock
     }
     
     session.requestSerializer.timeoutInterval = 60;
-}
-
-/**
- 返回成功后，对返回的object进行缓存。
-
- @param block 成功处理的block
- @param blockObj http返回成功的数据
- @param url http请求的全路径
- */
-- (void)handleSucessBlock:(void (^)(id))block
-                 BlockObj:(id)blockObj
-                      Url:(NSString *)url {
-    if (!httpCache) {
-        httpCache = [[YYCache alloc]initWithName:DDHttpCacheName];
-    }
-    ///YYCache 已经做了blockObj为空处理
-    [httpCache setObject:blockObj
-                  forKey:url];
-    if (block) {
-        block(blockObj);
-    }
 }
 
 @end
